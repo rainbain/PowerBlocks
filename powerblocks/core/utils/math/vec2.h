6ca893209a82013d2cf53e46b9010519a4bf2e1e
Sam Fitzsimons <samfitzsim64@gmail.com>
2025-08-23 13:52:57 -0500

    Added vec2 Function
    Added some of the functions from vec3 into vec2 as well.

--- powerblocks/core/utils/math/vec2.h
+++ powerblocks/core/utils/math/vec2.h
@@ -11,6 +11,8 @@
 
 #pragma once
 
+#include <math.h>
+
 typedef struct {
     int x;
     int y;
@@ -29,3 +31,21 @@ typedef struct {
 #define vec2i_new(x, y) ((vec2i){x, y})
 #define vec2s16_new(x, y) ((vec2s16){x, y})
 #define vec2_new(x, y) ((vec2){x, y})
+
+#define vec2_add(a, b) ((vec2){(a).x+(b).x, (a).y+(b).y})
+#define vec2_sub(a, b) ((vec2){(a).x-(b).x, (a).y-(b).y})
+#define vec2_dot(a, b) ((a).x * (b).x + (a).y * (b).y)
+#define vec2_magnitude_sq(a) vec2_dot(a, a)
+#define vec2_magnitude(a) (sqrtf(vec2_magnitude_sq(a)))
+
+#define vec2i_add(a, b) ((vec2i){(a).x+(b).x, (a).y+(b).y})
+#define vec2i_sub(a, b) ((vec2i){(a).x-(b).x, (a).y-(b).y})
+#define vec2i_dot(a, b) ((a).x * (b).x + (a).y * (b).y)
+#define vec2i_magnitude_sq(a) vec2_dot(a, a)
+#define vec2i_magnitude(a) (sqrtf(vec2_magnitude_sq(a)))
+
+#define vec2s16_add(a, b) ((vec2s16){(a).x+(b).x, (a).y+(b).y})
+#define vec2s16_sub(a, b) ((vec2s16){(a).x-(b).x, (a).y-(b).y})
+#define vec2s16_dot(a, b) ((a).x * (b).x + (a).y * (b).y)
+#define vec2s16_magnitude_sq(a) vec2_dot(a, a)
+#define vec2s16_magnitude(a) (sqrtf(vec2_magnitude_sq(a)))
