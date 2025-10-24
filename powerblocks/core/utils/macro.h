c38781e77399dd73155046782a8e073ec8d0b9fc
rainbain <samfitzsim64@gmail.com>
2025-10-23 23:49:24 -0500

    Added macros
    Start to add common C macros. Some people dont like these, I dont mind them.

---
+++ powerblocks/core/utils/macro.h
@@ -0,0 +1,15 @@
+/**
+ * @file macro.h
+ * @brief Various common macros.
+ *
+ * Provides macros for common math operations and bitwise.
+ *
+ * @author Samuel Fitzsimons (rainbain)
+ * @date 2025
+ * @license MIT (see LICENSE file)
+ */
+
+#pragma once
+
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
