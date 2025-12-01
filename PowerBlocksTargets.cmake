66c81a4893692483ab649b853555984327efe84e
rainbain <samfitzsim64@gmail.com>
2025-12-01 01:56:33 -0600

    Update PowerBlocksTargets.cmake

--- PowerBlocksTargets.cmake
+++ PowerBlocksTargets.cmake
@@ -16,5 +16,12 @@ add_library(PowerBlocks::Common ALIAS PowerBlocksCommon)
 
 set(FREERTOS_PATH ${CMAKE_CURRENT_LIST_DIR}/third_party/freertos)
 set(FATFS_PATH ${CMAKE_CURRENT_LIST_DIR}/third_party/fatfs)
+set(QRCODEGEN_PATH ${CMAKE_CURRENT_LIST_DIR}/third_party/qrcodegen)
+
+# Include DSP Microcode Tools
+add_subdirectory(
+    ${CMAKE_CURRENT_LIST_DIR}/tools/dspasm
+    ${CMAKE_CURRENT_BINARY_DIR}/tools/dspasm_build # Here to make cmake not complain about out of
+)
 
 add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/powerblocks" "${CMAKE_BINARY_DIR}/powerblocks")
